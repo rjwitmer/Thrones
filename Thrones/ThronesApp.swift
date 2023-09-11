0bee330767bc29aa82bc8a098ca4d9e4e5a774f9
Bob Witmer <bwitmer@golden.net>
2023-09-11 10:43:45 -0400

    Initial Commit

---
+++ Thrones/ThronesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThronesApp.swift
+//  Thrones
+//
+//  Created by Bob Witmer on 2023-09-11.
+//
+
+import SwiftUI
+
+@main
+struct ThronesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
